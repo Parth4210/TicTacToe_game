5f1030931468ff787ce12ba70a0f57edcb8bb114
Parth <58473966+Parth4210@users.noreply.github.com>
2026-04-16 00:33:05 +0530

    Added the Player factory function for creating players and Gameboard object

---
+++ index.js
@@ -0,0 +1,28 @@
+// Player creating factory function
+const Player = (name, sign) => {
+    this.name = name;
+    this.sign = sign;
+    return { name, sign }
+}
+
+// Creating the instances of players required
+const player1 = Player("P1", "X")
+const player2 = Player("P2", "O")
+
+// GameBoard object
+const GameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""]
+    const setValue = (index, sign) => {
+        board[index] = sign
+    }
+    const getValue = (index) => {
+        return board[index]
+    }
+    const resetBoard = ()=>{
+        for (let i = 0; i < board.length; i++) {
+            board[i] = ""
+        }
+    }
+})
+
+// GameController function for marking the board with the sign and futher checking for the winner
